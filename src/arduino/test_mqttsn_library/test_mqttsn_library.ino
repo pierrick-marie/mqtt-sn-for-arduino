fa1a464343524acf20ef0803ecbeb5715ceb9f7e
marie <me.pierrick@tutanota.com>
2018-06-05 23:14:13 +0200

    Init source code : * arduino clients * library

---
+++ src/arduino/test_mqttsn_library/test_mqttsn_library.ino
@@ -0,0 +1,12 @@
+#include <mqttsn-messages.h>
+#include <mqttsn.h>
+
+void setup() {
+ 
+ Serial.begin(9600); 
+}
+
+void loop() {
+ 
+ Serial.println("plop"); 
+}
---
+++ src/arduino/without-cycle/mqtt-client/decl.h
@@ -0,0 +1,6 @@
+
+uint16_t u16TopicPubID;
+uint8_t u8Counter;
+int cpt;
+uint8_t FrameBufferOut[API_FRAME_LEN];
+uint8_t gwAdd[8];
