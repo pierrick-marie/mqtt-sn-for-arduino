4554b78a99f375017f12be9017992b65f4a4a5e7
Pierrick MARIE <pierrick.marie@irit.fr>
2018-08-24 15:08:31 +0200

    Run script
    * add a new script with just the command line to run the program
    * keep the origial script to run the program on a Raspi

---
+++ src/gateway/run-pi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sleep 30
+
+killall java
+
+echo "" >> /home/pi/Documents/java-log.txt
+echo "$(date) - REBOOT" >> /home/pi/Documents/java-log.txt
+echo "" >> /home/pi/Documents/java-log.txt
+
+cd /home/pi/Downloads/MQTT_SN/
+
+java -cp ./lib/jssc-2.8.0.jar:./lib/hawtbuf-1.10.jar:./lib/mqtt-client-1.12.jar:./lib/hawtdispatch-1.21.jar:./lib/hawtdispatch-transport-1.21.jar:./bin gateway.Main >> /home/pi/Documents/java-log.txt 2>> /home/pi/Documents/java-error.txt
+
+
+
