d3203e280d10070421f4f6cd3040639a44cb52c3
Pierrick <mail@pierrickmarie.info>
2023-01-21 15:05:51 +0100

    Improve script to compile gateway

--- src/protocol/mqtt-sn/gateway/compile.sh
+++ src/protocol/mqtt-sn/gateway/compile.sh
@@ -4,10 +4,15 @@
 # Updated by Pierrick MARIE on 28/11/2018.
 
 if [ ! -d "bin" ]; then
-    mkdir bin
+	echo "Create bin directory"
+	mkdir ./bin
+else
+	echo "Clean bin directory"
+	rm -rf ./bin/*
 fi
 
-javac -d ./bin -sourcepath ./src/main/java -cp ./lib/jssc-2.8.0.jar:./lib/org.eclipse.paho.client.mqttv3-1.2.0.jar ./src/main/java/gateway/Main.java -Xlint:unchecked
+echo "Compile gateway"
 
+javac -d ./bin -sourcepath ./src/main/java -cp ./lib/jssc-2.8.0.jar:./lib/org.eclipse.paho.client.mqttv3-1.2.0.jar ./src/main/java/gateway/Main.java -Xlint:unchecked && echo "Done" && exit ;
 
-
+echo "Error during compilation"
